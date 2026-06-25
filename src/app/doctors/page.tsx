6ea3c238334d54cb606ea1bf7445bebf1c3f03f3
Rusu Iulian <iulianrusu29@gmail.com>
2026-06-25 09:56:56 +0300

    add database

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -1,7 +1,11 @@
 import DoctorS1 from '../components/DoctorS1'
 import DoctorS2 from '../components/DoctorS2'
+import { prisma } from '@/lib/prisma'
+import { Doctor } from '@/generated/prisma/client'
 
-export default function page() {
+export default async function page() {
+  const doctors = await prisma.doctor.findMany()
+
   return (
     <main className="min-h-screen px-4 py-12 md:p-20 flex flex-col gap-8">
       <div className="max-w-6xl mx-auto w-full">
@@ -9,10 +13,11 @@ export default function page() {
       </div>
 
       <section className="gap-8 flex flex-col max-w-6xl mx-auto bg-[#8900F2] p-6 md:p-20 rounded-2xl shadow-lg w-full">
-        <DoctorS1 name="Dr. Eliza Reed" desc="Medic Dentist – Estetică și Protetică Dentară" image="/doctor1.png" />
-        <DoctorS2 name="Dr. Adrian Dumitrescu" desc="Medic Primar Chirurgie Orală și Implantologie" image="/doctor2.png" />
-        <DoctorS1 name="Dr. Elena Radu" desc="Medic Dentist – Ortodonție și Pediatrică" image="/doctor4.png" />
-        <DoctorS2 name="Dr. Mihaela Costin" desc="Medic Dentist – Specializată în Profilaxie și Estetică Dentară" image="/doctor5.png" />
+        {doctors.map((doctor: Doctor, index: number) =>
+          index % 2 === 0
+            ? <DoctorS1 key={doctor.id} name={doctor.name} desc={doctor.desc} image={doctor.image} />
+            : <DoctorS2 key={doctor.id} name={doctor.name} desc={doctor.desc} image={doctor.image} />
+        )}
       </section>
     </main>
   )
